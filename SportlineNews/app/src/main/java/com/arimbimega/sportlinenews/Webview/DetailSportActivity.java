2fa7ab57c6fc26b95b14e3b2a087d1c427510d43
unknown <arimbimega1@gmail.com>
2021-07-01 21:11:49 +0700

    Add progressbar and back button in toolbar

--- SportlineNews/app/src/main/java/com/arimbimega/sportlinenews/Webview/DetailSportActivity.java
+++ SportlineNews/app/src/main/java/com/arimbimega/sportlinenews/Webview/DetailSportActivity.java
@@ -1,12 +1,16 @@
 package com.arimbimega.sportlinenews.Webview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.arimbimega.sportlinenews.Model.Articles;
 import com.arimbimega.sportlinenews.R;
@@ -15,13 +19,21 @@ public class DetailSportActivity extends AppCompatActivity {
 
     WebView webView;
 
+    ProgressBar progressBar;
+
+    String UrlNews;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_sport);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Articles articles = getIntent().getParcelableExtra("articlesArrayList");
-        String UrlNews = articles.getUrl();
+        UrlNews = articles.getUrl();
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setMax(100);
 
         webView = findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
@@ -34,8 +46,35 @@ public class DetailSportActivity extends AppCompatActivity {
 
     private class Callback extends WebViewClient {
         @Override
-        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
-            return false;
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(UrlNews);
+            progressBar.setProgress(0);
+            return true;
         }
+
+        @Override
+        public void onPageStarted(WebView view, String urlStart, Bitmap favicon) {
+            super.onPageStarted(view, urlStart, favicon);
+            UrlNews = urlStart;
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(view.GONE);
+            invalidateOptionsMenu();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
