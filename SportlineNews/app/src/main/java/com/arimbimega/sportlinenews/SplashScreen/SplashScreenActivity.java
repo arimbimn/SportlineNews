2f25b6f869915decc8dba01488906b0de0f43fed
unknown <arimbimega1@gmail.com>
2021-07-03 23:22:14 +0700

    Adding Splash Screen to the app

---
+++ SportlineNews/app/src/main/java/com/arimbimega/sportlinenews/SplashScreen/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.arimbimega.sportlinenews.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.arimbimega.sportlinenews.MainActivity;
+import com.arimbimega.sportlinenews.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
